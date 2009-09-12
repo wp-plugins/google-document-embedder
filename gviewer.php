3ae46dc7ee0f769ceeb986c8d902af4352d25367
k3davis <k3davis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-12 15:42:11 +0000

    git-svn-id: https://plugins.svn.wordpress.org/google-document-embedder/tags/0.2@154079 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ gviewer.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+Plugin Name: Google Doc Embedder
+Plugin URI: http://wordpress.org/#
+Description: Lets you embed PDF files and PowerPoint presentations in a web page using the Google Document Viewer.
+Author: Kevin Davis
+Version: 0.2
+Author URI: http://www.davismetro.com/
+*/
+
+// [gview file="http://path.to/file.pdf" save="1" width="600" height="500"]
+function gviewer_func($atts) {
+	extract(shortcode_atts(array(
+		'file' => '',
+		'save' => '1',
+		'width' => '600',
+		'height' => '500'
+	), $atts));
+
+	$code=<<<HERE
+	<iframe src="http://docs.google.com/gview?url=%U%&embedded=true" style="width:%W%px; height:%H%px;" frameborder="0"></iframe>
+HERE;
+
+    $code = str_replace("%U%", $file, $code);
+	$code = str_replace("%W%", $width, $code);
+	$code = str_replace("%H%", $height, $code);
+	if ($save == "1") {
+	  $code .= "<p><a href=\"$file\" target=\"_blank\">download file</a></p>";
+	}
+
+    return $code;
+}
+
+add_shortcode('gview', 'gviewer_func');
+
+?>
