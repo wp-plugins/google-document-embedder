34b67924fee076ab17a61a8d461edb99bdb1ee65
k3davis <k3davis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-05 04:45:28 +0000

    git-svn-id: https://plugins.svn.wordpress.org/google-document-embedder/tags/1.9.8@355544 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ options.php
@@ -0,0 +1,311 @@
+﻿<?php
+include_once('gde-functions.php');
+
+global $gdeoptions;
+$himg = plugins_url(plugin_basename(dirname(__FILE__))).'/help.png';
+
+if(isset($_REQUEST['defaults'])) {
+
+	$set = gde_init('reset');
+	$gdeoptions = get_option('gde_options');
+	gde_showMessage("Options reset to defaults");
+
+} elseif(isset($_REQUEST['submit'])) {
+
+	// change user defaults
+	if(isset($_POST['show_dl'])) {
+		$gdeoptions['show_dl'] = "yes";
+	} else {
+		$gdeoptions['show_dl'] = "no";
+	}
+	if(isset($_POST['restrict_dl'])) {
+		$gdeoptions['restrict_dl'] = "yes";
+	} else {
+		$gdeoptions['restrict_dl'] = "no";
+	}
+	if(isset($_POST['default_width'])) {
+		$neww = $_POST['default_width'];
+		if (strlen($neww) > 0) $gdeoptions['default_width'] = gde_sanitizeOpt($neww, $_POST['width_type']);
+	}
+	if(isset($_POST['width_type'])) {
+		$gdeoptions['width_type'] = $_POST['width_type'];
+	}
+	if(isset($_POST['default_height'])) {
+		$newh = $_POST['default_height'];
+		if (strlen($newh) > 0) $gdeoptions['default_height'] = gde_sanitizeOpt($newh, $_POST['height_type']);
+	}
+	if(isset($_POST['height_type'])) {
+		$gdeoptions['height_type'] = $_POST['height_type'];
+	}
+	if(isset($_POST['default_lang'])) {
+		$gdeoptions['default_lang'] = $_POST['default_lang'];
+	}
+	if(isset($_POST['gdet_i'])) { $newgdet .= "i"; }
+	if(isset($_POST['gdet_p'])) { $newgdet .= "p"; }
+	if(isset($_POST['gdet_z'])) { $newgdet .= "z"; }
+	if(isset($_POST['gdet_n'])) { $newgdet .= "n"; }
+	$gdeoptions['restrict_tb'] = $newgdet;
+	if(isset($_POST['link_text'])) {
+		$newt = $_POST['link_text'];
+		if (strlen(utf8_decode($newt))) $gdeoptions['link_text'] = $newt;
+	}
+	if(isset($_POST['link_pos'])) {
+		$gdeoptions['link_pos'] = $_POST['link_pos'];
+	}
+	if(isset($_POST['link_func'])) {
+		$gdeoptions['link_func'] = $_POST['link_func'];
+	}
+	if(isset($_POST['disable_proxy'])) {
+		$gdeoptions['disable_proxy'] = $_POST['disable_proxy'];
+	}
+	if(isset($_POST['bypass_check'])) {
+		$gdeoptions['bypass_check'] = "yes";
+	} else {
+		$gdeoptions['bypass_check'] = "no";
+	}
+	if(isset($_POST['ignore_conflicts'])) {
+		$gdeoptions['ignore_conflicts'] = "yes";
+	} else {
+		$gdeoptions['ignore_conflicts'] = "no";
+	}
+	if(isset($_POST['suppress_beta'])) {
+		$gdeoptions['suppress_beta'] = "yes";
+	} else {
+		$gdeoptions['suppress_beta'] = "no";
+	}
+	
+	update_option('gde_options', $gdeoptions);
+	gde_showMessage("Options updated");
+}
+?>
+<div class="wrap">
+<?php
+echo "<h2>".__('Google Doc Embedder Settings')."</h2>";
+?>
+
+<form action="" method="post">
+<?php wp_nonce_field('update-options'); ?>
+
+<div id="poststuff" class="metabox-holder">
+	<div class="sm-padded" >
+		<div id="post-body-content" class="has-sidebar-content">
+			<div class="meta-box-sortabless">
+				<div id="gde_vieweroptions" class="postbox">
+
+				<h3 class="hndle"><span>Viewer Options</span></h3>
+				<div class="inside">
+				
+				<table class="form-table">
+<tr valign="top">
+<th scope="row">Viewer Selection</th>
+<td><div style="float:right;"><a href="<?php echo GDE_VIEWOPT_URL; ?>" target="_blank" title="Help"><img src="<?php echo $himg; ?>"></a></div><?php gde_showRadio('no', 'dp1', 'disable_proxy', gde_t('Enhanced Viewer')); ?><br />
+<em>Use this option to improve IE compatibility and enable toolbar customization.</em><br/>
+<?php gde_showRadio('yes', 'dp2', 'disable_proxy', gde_t('Google Standard Viewer')); ?><br />
+<em>Use this option if you experience problems with the Enhanced Viewer.</em>
+</td>
+</tr>
+<tr valign="top">
+<th scope="row">Default Size</th>
+<td><strong>Width </strong><input type="text" size="5" name="default_width" value="<?php echo $gdeoptions['default_width']; ?>" />  <select name="width_type">
+<?php gde_showOption('px', 'width_type', gde_t('px')); ?>
+<?php gde_showOption('pc', 'width_type', gde_t('%')); ?>
+</select> &nbsp;&nbsp;&nbsp;&nbsp;
+<strong>Height </strong><input type="text" size="5" name="default_height" value="<?php echo $gdeoptions['default_height']; ?>" /> <select name="height_type">
+<?php gde_showOption('px', 'height_type', gde_t('px')); ?>
+<?php gde_showOption('pc', 'height_type', gde_t('%')); ?>
+</select></td>
+</tr>
+<tr valign="top">
+<th scope="row">Default Language</th>
+<td><select name="default_lang">
+
+<?php gde_showOption('cs', 'default_lang', gde_t('&#268;esky')); ?>
+<?php gde_showOption('sr', 'default_lang', gde_t('&#x0421;&#x0440;&#x043F;&#x0441;&#x043A;&#x0438;')); ?>
+<?php gde_showOption('uk', 'default_lang', gde_t('&#x0423;&#x043A;&#x0440;&#x0430;&#x0457;&#x043D;&#x0441;&#x044C;&#x043A;&#x0430;')); ?>
+<?php gde_showOption('el', 'default_lang', gde_t('&Epsilon;&lambda;&lambda;&eta;&nu;&iota;&kappa;ά')); ?>
+<?php gde_showOption('ar', 'default_lang', gde_t('Arabic')); ?>
+<?php gde_showOption('in', 'default_lang', gde_t('Bahasa Indonesia')); ?>
+<?php gde_showOption('ca', 'default_lang', gde_t('Catal&agrave;')); ?>
+<?php gde_showOption('da', 'default_lang', gde_t('Dansk')); ?>
+<?php gde_showOption('de', 'default_lang', gde_t('Deutsch')); ?>
+<?php gde_showOption('en_GB', 'default_lang', gde_t('English (UK)')); ?>
+<?php gde_showOption('en_US', 'default_lang', gde_t('English (US)')); ?>
+<?php gde_showOption('es', 'default_lang', gde_t('Espa&ntilde;ol')); ?>
+<?php gde_showOption('fil', 'default_lang', gde_t('Filipino')); ?>
+<?php gde_showOption('fr', 'default_lang', gde_t('Fran&ccedil;ais')); ?>
+<?php gde_showOption('iw', 'default_lang', gde_t('Hebrew')); ?>
+<?php gde_showOption('hr', 'default_lang', gde_t('Hrvatski')); ?>
+<?php gde_showOption('it', 'default_lang', gde_t('Italiano')); ?>
+<?php gde_showOption('lv', 'default_lang', gde_t('Latvie&scaron;u')); ?>
+<?php gde_showOption('lt', 'default_lang', gde_t('Lietuvių')); ?>
+<?php gde_showOption('hu', 'default_lang', gde_t('Magyar')); ?>
+<?php gde_showOption('nl', 'default_lang', gde_t('Nederlands')); ?>
+<?php gde_showOption('no', 'default_lang', gde_t('Norsk (bokmål)')); ?>
+<?php gde_showOption('pl', 'default_lang', gde_t('Polski')); ?>
+<?php gde_showOption('pt_BR', 'default_lang', gde_t('Portugu&ecirc;s (Brasil)')); ?>
+<?php gde_showOption('pt_PT', 'default_lang', gde_t('Portugu&ecirc;s (Portugal)')); ?>
+<?php gde_showOption('ro', 'default_lang', gde_t('Rom&acirc;n&#x0103;')); ?>
+<?php gde_showOption('sl', 'default_lang', gde_t('Sloven&#x0161;&#x010D;ina')); ?>
+<?php gde_showOption('sk', 'default_lang', gde_t('Slovensk&yacute;')); ?>
+<?php gde_showOption('fi', 'default_lang', gde_t('Suomi')); ?>
+<?php gde_showOption('sv', 'default_lang', gde_t('Svenska')); ?>
+<?php gde_showOption('tr', 'default_lang', gde_t('T&uuml;rk&ccedil;e')); ?>
+<?php gde_showOption('vi', 'default_lang', gde_t('Tiếng Việt')); ?>
+<?php gde_showOption('ru', 'default_lang', gde_t('Русский')); ?>
+<?php gde_showOption('bg', 'default_lang', gde_t('български')); ?>
+<?php gde_showOption('mr', 'default_lang', gde_t('मराठी')); ?>
+<?php gde_showOption('hi', 'default_lang', gde_t('हिन्दी')); ?>
+<?php gde_showOption('bn', 'default_lang', gde_t('বাংলা')); ?>
+<?php gde_showOption('gu', 'default_lang', gde_t('ગુજરાતી')); ?>
+<?php gde_showOption('or', 'default_lang', gde_t('ଓଡିଆ')); ?>
+<?php gde_showOption('ta', 'default_lang', gde_t('தமிழ்')); ?>
+<?php gde_showOption('te', 'default_lang', gde_t('తెలుగు')); ?>
+<?php gde_showOption('kn', 'default_lang', gde_t('ಕನ್ನಡ')); ?>
+<?php gde_showOption('ml', 'default_lang', gde_t('മലയാളം')); ?>
+<?php gde_showOption('th', 'default_lang', gde_t('ภาษาไทย')); ?>
+<?php gde_showOption('zh_CN', 'default_lang', gde_t('中文（简体）')); ?>
+<?php gde_showOption('zh_TW', 'default_lang', gde_t('中文（繁體）')); ?>
+<?php gde_showOption('ja', 'default_lang', gde_t('日本語')); ?>
+<?php gde_showOption('ko', 'default_lang', gde_t('한국어')); ?>
+
+</select></td>
+</tr>
+<?php
+if ($gdeoptions['disable_proxy'] == "no") {
+?>
+<tr valign="top">
+<th scope="row">Hide Toolbar Buttons</th>
+<td><?php gde_showCheckTb('gdet_i', gde_t('Google Logo')); ?>
+<?php gde_showCheckTb('gdet_p', gde_t('Single/Double Page View')); ?>
+<?php gde_showCheckTb('gdet_z', gde_t('Zoom In/Out')); ?>
+<?php gde_showCheckTb('gdet_n', gde_t('Open in New Window')); ?>
+</td>
+</tr>
+<?php } ?>
+</table>
+				
+				</div>
+				</div>
+				</div>
+
+
+<div id="gde_linkoptions" class="postbox">
+
+				<h3 class="hndle"><span>Download Link Options</span></h3>
+				<div class="inside">
+<table class="form-table">
+<tr valign="top">
+<td colspan="2"><div style="float:right;"><a href="<?php echo GDE_LINKOPT_URL; ?>" target="_blank" title="Help"><img src="<?php echo $himg; ?>"></a></div><?php gde_showCheck('show_dl', gde_t('Display the download link by default')); ?><br/>
+<?php gde_showCheck('restrict_dl', gde_t('Only display download link to logged in users')); ?></td>
+</tr>
+<tr valign="top">
+<th scope="row">Link Text</th>
+<td><input type="text" size="50" name="link_text" value="<?php echo $gdeoptions['link_text']; ?>" /><br/>
+<em>You can further customize text using these dynamic replacements:</em><br/>
+<code>%FN</code> : filename &nbsp;&nbsp;&nbsp;
+<code>%FT</code> : file type &nbsp;&nbsp;&nbsp;
+<code>%FS</code> : file size</td>
+</tr>
+<tr valign="top">
+<th scope="row">Link Position</th>
+<td><select name="link_pos">
+<?php gde_showOption('above', 'link_pos', gde_t('Above Viewer')); ?>
+<?php gde_showOption('below', 'link_pos', gde_t('Below Viewer')); ?>
+</select>
+</td>
+</tr>
+<r valign="top">
+<th scope="row">Link Behavior</th>
+<td><select name="link_func">
+<?php gde_showOption('default', 'link_func', gde_t('Browser Default')); ?>
+<?php gde_showOption('force', 'link_func', gde_t('Force Download')); ?>
+<?php gde_showOption('force-mask', 'link_func', gde_t('Force Download (Mask URL)')); ?>
+</select>
+</td>
+</tr>
+<tr valign="top">
+<td colspan="2">
+</div></td>
+</tr>
+</tr>
+</table>
+				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+				
+<p class="submit" style="padding-bottom:10px;">
+<div style="font-size:11px;margin-top:-40px;padding-bottom:20px;padding-left:5px;">
+<strong><a style="text-decoration:none;" href="javascript:;" onmousedown="if(document.getElementById('advopt').style.display == 'none'){ document.getElementById('advopt').style.display = 'block'; }else{ document.getElementById('advopt').style.display = 'none'; }">[ + ]</a> Advanced Options</strong> <a href="<?php echo GDE_ADVOPT_URL; ?>" target="_blank"><img src="<?php echo $himg; ?>"></a><br />
+<div id="advopt" style="display:none;">
+<?php gde_showCheck('bypass_check', gde_t('Disable internal error checking')); ?><br />
+<?php gde_showCheck('ignore_conflicts', gde_t('Disable plugin conflict warnings')); ?><br />
+<?php gde_showCheck('suppress_beta', gde_t('Disable beta version notifications')); ?>
+</div>
+</div>
+
+<input type="hidden" id="user-id" name="user_ID" value="<?php echo (int) $user_ID ?>" />
+<span id="autosave"></span>
+<input class="button-primary" type="submit" name="submit" value="<?php gde_e('Save Options') ?>" />
+&nbsp;&nbsp;&nbsp;
+<input class="button-secondary" type="submit" name="defaults" value="<?php gde_e('Reset to Defaults') ?>" onClick="javascript:return confirm('Are you sure you want to reset all settings to defaults?')" />
+</p>
+
+</form>
+
+</div>
+
+<?php
+
+function gde_showRadio($value, $id, $option, $title) {
+	global $gdeoptions;
+	if ($gdeoptions[$option] == $value) { $chk = ' checked="checked"'; }
+?>
+<input type="radio" name="<?php echo $option; ?>" value="<?php echo $value; ?>" id="<?php echo $id; ?>"<?php echo $chk; ?> />
+<label for="<?php echo $id; ?>"><strong><?php gde_e($title) ?></strong></label>
+<?php
+}
+
+function gde_showCheck($option, $title, $link = NULL) {
+	global $gdeoptions;
+	if ($gdeoptions[$option] == "yes") { $chk = ' checked="checked"'; }
+?>
+<input type="checkbox" name="<?php echo $option; ?>" value="1" id="<?php echo $option; ?>"<?php echo $chk; ?> />
+<label for="<?php echo $option; ?>"><?php gde_e($title) ?></label>
+<?php
+	if ($link) {
+		echo ' (<a href="'.$link.'" target="_blank">info</a>)';
+	}
+}
+
+function gde_showCheckTb($option, $title) {
+	global $gdeoptions;
+	$gdet = $gdeoptions['restrict_tb'];
+	
+	$option = str_replace("gdet_", "", $option);
+	
+	if (strstr($gdet, $option) !== false) { $chk = ' checked="checked"'; }
+?>
+<input type="checkbox" name="gdet_<?php echo $option; ?>" value="1" id="gdet_<?php echo $option; ?>"<?php echo $chk; ?> />
+<label for="gdet_<?php echo $option; ?>"><?php gde_e($title) ?></label>
+<?php
+}
+
+function gde_showOption($value, $option, $title) {
+	global $gdeoptions;
+	if ($gdeoptions[$option] == $value) { $chk = ' selected="yes"'; }
+?>
+<option value="<?php echo $value; ?>"<?php echo $chk; ?>><?php echo $title; ?></option>
+<?php
+}
+
+function gde_showMessage($message, $type='updated') {
+	if($type == 'updated') $class = 'updated fade';
+	elseif($type == 'error') $class = 'updated error';
+	else $class = $type;
+	
+	print '<div id="message" class="'.$class.'"><p>' . __($message, basename(dirname(__FILE__))) . '</p></div>';
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,155 @@
+=== Google Doc Embedder ===
+Contributors: k3davis
+Donate link: http://pledgie.com/campaigns/6048
+Tags: doc, docx, pdf, pps, ppt, xls, psd, svg, tiff, office, powerpoint, google, google docs
+Requires at least: 2.8
+Tested up to: 3.1
+Stable tag: trunk
+
+Lets you embed MS Office, PDF, TIFF, and many other file types in a web page using the Google Docs Viewer (no Flash or PDF browser plug-ins required).
+
+== Description ==
+
+Google Doc Embedder lets you embed several types of files into your WordPress pages using the Google Docs Viewer - allowing inline viewing (and optional downloading) of the following file types, with no Flash or PDF browser plug-ins required:
+
+* Adobe PDF
+* Microsoft Word (DOC/DOCX)
+* Microsoft PowerPoint (PPT/PPTX/PPS)
+* Microsoft Excel (XLS/XLSX)
+* TIFF Images
+* Apple Pages (PAGES)
+* Adobe Illustrator (AI)
+* Adobe Photoshop (PSD)
+* Autodesk AutoCad (DXF)
+* Scalable Vector Graphics (SVG)
+* PostScript (EPS/PS)
+* TrueType (TTF)
+* XML Paper Specification (XPS)
+
+Similar to services like Scribd, Google Doc Embedder will allow you to embed these files directly into your page or post, not requiring the user to have Microsoft Word, Adobe Reader, PowerPoint, or other software installed to view the contents. Unlike Scribd, the files do not need to be uploaded to any service first - including Google Documents - but can exist anywhere publicly accessible on your site or the internet.
+
+Note: Use of this plug-in implies your agreement with Google's published <a href="http://docs.google.com/viewer/TOS?hl=en" target="_blank">Terms of Service</a>.
+
+== Installation ==
+
+1. Upload the entire `google-document-embedder` folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Done.
+
+For basic usage, please see the FAQ. For detailed usage, see the <a href="http://www.davismetro.com/gde/usage/">web site</a>.
+
+Go to "GDE Settings" (under "Settings" in the admin panel) to change defaults, or override individually using the shortcode syntax in the FAQ.
+
+== Frequently Asked Questions ==
+
+= What file types can be embedded? =
+This plug-in currently can embed the following:
+
+* Adobe PDF
+* Microsoft Word (DOC/DOCX)
+* Microsoft PowerPoint (PPT/PPTX/PPS)
+* Microsoft Excel (XLS/XLSX)
+* TIFF Images
+* Apple Pages (PAGES)
+* Adobe Illustrator (AI)
+* Adobe Photoshop (PSD)
+* Autodesk AutoCad (DXF)
+* Scalable Vector Graphics (SVG)
+* PostScript (EPS/PS)
+* TrueType (TTF)
+* XML Paper Specification (XPS)
+
+The file to embed must first be publicly available somewhere on the internet. You can upload it to your WordPress site using the standard techniques, or link to a file on another site.
+
+= How do I embed a file in my page or post? =
+Use the custom shortcode `[gview]` to embed the file, as shown:
+
+`[gview file="http://url.to/file.pdf"]`
+
+Note: the `file=` attribute (pointing to the full URL of the file) is **required**.
+
+Optional attributes:
+
+* `save=` : Set to 0 if you wish to suppress the direct download link to the file under the embedded viewer (1 for on, by default)
+* `width=` : To override the default width of the viewer, enter a new width value - e.g., "400" (px) or "80%"
+* `height=` : To override the default height of the viewer, enter a new height value - e.g., "400" (px) or "80%"
+
+For a list of all available attributes, see <a href="http://www.davismetro.com/gde/usage/">Usage</a>.
+
+= Will it embed files that are password-protected  or stored in protected folders/sites? =
+Most likely, no. If your file requires a login to view - such as being saved in a password-protected directory, or behind a firewall (on your intranet, etc.), the viewer will probably not be able to access the file. For files stored on Google Docs, the viewer will prompt you to log in first, which most users presumably couldn't do. This is what is meant above, that the document should be "publicly available." Please save the file in a publicly accessible location for best results.
+
+= Nothing is showing up! What do I do? =
+View the source on the web page where you've embedded the viewer. In order to degrade gracefully in case an error occurs, error messages will be inserted as HTML comments in these pages at the spot the viewer is called.
+
+= I wish the plug-in had feature XYZ... =
+That's not a question ;) but if you have any particular ideas on further development of this plug-in, please post <a href="http://wordpress.org/tags/google-document-embedder?forum_id=10#postform">on the forum</a> or privately using the <a href="http://www.davismetro.com/gde/support/">support form</a> and I'll see what I can do.
+
+== Screenshots ==
+
+1. Default appearance of embedded viewer
+2. Settings page
+
+== Changelog ==
+
+= 1.9.8 =
+* Added: Viewer support for multiple new file types
+* Changed: Workaround for NextGEN Gallery incompatibility (thanks alex)
+* Changed: Defaults to Standard Viewer (IE problem is fixed!)
+
+= 1.9.7 =
+* Fixed: Minor compatibility issue with some PHP versions
+* Changed: Confirmed compatibility with WP 3.1
+
+= 1.9.6 =
+* Changed: Removed min-width restriction of viewer (thanks Amanda)
+* Fixed: Enhanced Viewer failed in hardened PHP configs (thanks Waseem)
+* Fixed: Force Download option failed in hardened PHP configs
+* Fixed: Options page layout quirks in lower resolutions
+* Fixed: Options page PHP parse error on XAMPP (thanks John)
+
+= 1.9.5 =
+* Added: Support for Word documents (DOC, DOCX)
+* Added: Default language option for viewer
+* Added: Revived ability to hide selected viewer toolbar buttons (from 1.7)
+* Changed: IE now supported by "enhanced viewer" proxy option (ie-warn removed)
+* Changed: Improved options organization
+* Fixed: Filenames with spaces wouldn't load
+* Fixed: Suppress beta notification option not honored
+
+= 1.9.4 =
+* Added: Option to restrict download link to logged in users (thanks kris)
+* Added: Compatibility with WP 3.0
+
+= 1.9.3 =
+* Added: Support for PPS files (thanks Dan)
+* Changed: Simplified default IE Warning text
+
+= 1.9.2 =
+* Fixed: "Turn off beta notifications" needlessly still checked for beta version
+* Confirmed WP 2.9 compatibility
+* Beta delivery test
+
+= 1.9.1 =
+* Fixed: Options not saved in some instances due to variable collision (thanks gadgetto)
+* Fixed: Options not saved when plugin is reactivated
+* Changed: Default width now 100% (existing setting will be preserved)
+* Added: Notification of beta versions - <a href="http://davismetro.com/gde/beta-program" target="_blank">more info</a>
+
+= 1.9 =
+* Added: Revealed more troubleshooting options (under "Advanced Options")
+* Fixed: No longer relies on cURL for any function
+* Changed: Function overhaul for general efficiency and reduced database calls
+* Removed: WP 2.5 compatibility. Now requires WordPress 2.8+.
+
+= 1.8.2 = 
+* Fixed: Fatal PHP error if cURL library not active (WAMPServer, etc.)
+
+= 1.8.1 =
+* Added: Temporary (I hope) workaround option to insert help statement for users of IE8 - <a href="http://davismetro.com/gde/ie8" target="_blank">more info</a>
+
+= 1.8 =
+* Added: Ability to set height/width to percentage (thanks eturfboer)
+* Fixed: Compatibility with PHP 5.3+, various function tuning
+
+<a href="http://davismetro.com/gde/changelog/" target="_blank">Full history...</a>
