0990a18c537c24b9cdd4b72abecd86d1d8ae56b5
k3davis <k3davis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-08 11:59:31 +0000

    git-svn-id: https://plugins.svn.wordpress.org/google-document-embedder/tags/2.2@420552 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/editor_plugin.js
@@ -0,0 +1,39 @@
+(function() {
+	tinymce.PluginManager.requireLangPack('gde');
+	tinymce.create('tinymce.plugins.GDEPlugin', {
+		init : function(ed,url) {
+			ed.addCommand('mceGDE', function() {
+				ed.windowManager.open( {
+					file : url + '/../gde-dialog.php',
+					width : 420 + parseInt(ed.getLang('gde.delta_width',0)),
+					height : 540 + parseInt(ed.getLang('gde.delta_height',0)),
+					inline : 1}, {
+						plugin_url : url,
+						some_custom_arg : 'custom arg'
+					}
+				)}
+			);
+			ed.addButton('gde', {
+				title : 'Google Doc Embedder',
+				cmd : 'mceGDE',
+				image : url + '/../img/gde-button.png'
+			});
+			ed.onNodeChange.add
+				(function(ed,cm,n) {
+					cm.setActive('gde',n.nodeName=='IMG')
+				})
+		},
+		createControl : function(n,cm) {
+			return null
+		},
+		getInfo : function() { 
+			return { 
+				longname : 'Google Doc Embedder',
+				author : 'Kevin Davis',
+				authorurl : 'http://davismetro.com/gde',
+				infourl : 'http://davismetro.com/gde',
+				version : "1.0"}
+		}
+	});
+	tinymce.PluginManager.add('gde',tinymce.plugins.GDEPlugin)
+})();
